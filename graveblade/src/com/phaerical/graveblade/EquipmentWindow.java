db049f5e10a81d417657bea048816c7785034c23
Edward Truong <edward.truong.12@gmail.com>
2013-08-27 00:41:22 -0400

    Added basic inventory layout

--- graveblade/src/com/phaerical/graveblade/EquipmentWindow.java
+++ graveblade/src/com/phaerical/graveblade/EquipmentWindow.java
@@ -4,12 +4,15 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton.ImageButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -52,25 +55,30 @@ public class EquipmentWindow extends Window
 		padRight (35);
 		left ();
 		
-		tooltip = new Label ("", skin, "small-boxed");
+		right ();
+		
+		tooltip = new Label ("", skin, "tooltip");
 		tooltip.setAlignment (Align.center, Align.left);
-		tooltip.setSize (200, 180);
 		tooltip.setPosition(0, 0);
 		tooltip.setVisible (false);
 		
-		ArrayIterator<Item> iter = new ArrayIterator<Item> (hero.getEquipment ());
+		TextureAtlas itemAtlas = new TextureAtlas (Gdx.files.internal ("sprites/items.pack"));
 		
-		while (iter.hasNext ())
+		for (int i = 0; i < 24; i++)
 		{
-			Item item = iter.next ();
+			ImageButtonStyle style = new ImageButtonStyle ();
+			ImageButton btn = new ImageButton (style);
 			
-			item.addListener (new InputListener ()
+			style.up = skin.getDrawable("box");
+			style.imageUp = new SpriteDrawable (itemAtlas.createSprite ("sword1"));
+			
+			btn.addListener (new InputListener ()
 			{
 				@Override
 				public void enter (InputEvent event, float x, float y, int pointer, Actor fromActor)
 				{
-					Item i = (Item) event.getListenerActor ();
-					tooltip.setText (i.getTooltip ());
+					tooltip.setText ("hi");
+					tooltip.setSize (tooltip.getPrefWidth() + 30, tooltip.getPrefHeight() + 30);
 					tooltip.setVisible (true);
 				}
 				
@@ -89,7 +97,7 @@ public class EquipmentWindow extends Window
 					// Handle tooltip clipping
 					if (tX + tooltip.getWidth () > event.getStage().getWidth ())
 					{
-						tX = x + event.getListenerActor().getX() - tooltip.getWidth () / 2 - 10;
+						tX = x + event.getListenerActor().getX() - tooltip.getWidth () / 2 + 10;
 					}
 					
 					if (tY < 0)
@@ -103,16 +111,74 @@ public class EquipmentWindow extends Window
 				}
 			});
 			
-			add (item).size(64, 64).spaceRight (50).row ();
+			add (btn).size (64, 64).spaceRight (4).spaceBottom (4);
+			
+			if ((i + 1) % 6 == 0)
+			{
+				row ();
+			}
 		}
 		
-		right ();
+		row ();
+		
+		ArrayIterator<Item> iter = new ArrayIterator<Item> (hero.getEquipment ());
+		
+		/*
+		while (iter.hasNext ())
+		{
+			Item item = iter.next ();
+			
+			item.addListener (new InputListener ()
+			{
+				@Override
+				public void enter (InputEvent event, float x, float y, int pointer, Actor fromActor)
+				{
+					Item i = (Item) event.getListenerActor ();
+					tooltip.setText (i.getTooltip ());
+					tooltip.setSize (tooltip.getPrefWidth() + 30, tooltip.getPrefHeight() + 30);
+					tooltip.setVisible (true);
+				}
+				
+				@Override
+				public void exit (InputEvent event, float x, float y, int pointer, Actor toActor)
+				{
+					tooltip.setVisible (false);
+				}
+				
+				@Override
+				public boolean mouseMoved (InputEvent event, float x, float y)
+				{
+					float tX = x + event.getListenerActor().getX() + tooltip.getWidth () / 2 + 10;
+					float tY = y + event.getListenerActor().getY() - tooltip.getHeight () / 2;
+					
+					// Handle tooltip clipping
+					if (tX + tooltip.getWidth () > event.getStage().getWidth ())
+					{
+						tX = x + event.getListenerActor().getX() - tooltip.getWidth () / 2 + 10;
+					}
+					
+					if (tY < 0)
+					{
+						tY = 0;
+					}
+					
+					tooltip.setPosition (tX, tY);
+					
+					return true;
+				}
+			});
+			
+			add (item).size(64, 64).spaceRight (50);
+		}*/
+		
+		
 	}
 	
 	public void show ()
 	{
 		addAction (Actions.moveTo (100, 80, 0.15f));
-		getStage().addActor(tooltip);
+		getStage().addActor (tooltip);
+		toFront ();
 		open = true;
 	}
 	
